ee3cdbc8ae923b8ecc7f526fa8223481a3a2f5b0
Gabriel Anglada <Gabriel@MacBook-Pro-de-Admin.local>
2018-04-16 10:08:17 +0200

    some design minor improvements & tabindez management

--- src/app/pages/welcome_form/form.ts
+++ src/app/pages/welcome_form/form.ts
@@ -5,55 +5,60 @@ import {DataService} from "../../services/data.service";
 import {AlertService} from "../../services/alert.service";
 
 @Component({
-    selector: 'page-welcome-form',
-    templateUrl: 'form.html'
+  selector: 'page-welcome-form',
+  templateUrl: 'form.html'
 })
 export class FormPage {
 
-    @ViewChild(Slides) slides: Slides;
+  @ViewChild(Slides) slides: Slides;
 
-    public form: FormGroup;
+  public form: FormGroup;
 
-    constructor(private formBuilder: FormBuilder,
-                private dataService: DataService,
-                private alertService: AlertService) {
-        this._createForm();
-    }
+  constructor(private formBuilder: FormBuilder,
+              private dataService: DataService,
+              private alertService: AlertService) {
+    this._createForm();
+  }
 
-    ionViewDidEnter() {
-        // this.slides.lockSwipes(true);
-    }
+  ionViewDidEnter() {
+    this.slides.lockSwipes(true);
+  }
 
 
-    onBackClick() {
-        this.slides.slidePrev();
-    }
+  onBackClick() {
+    this.slides.lockSwipes(false);
+    this.slides.slidePrev();
+    this.slides.lockSwipes(true);
+  }
 
-    onForwardClick() {
-        this.slides.slideNext();
-    }
+  onForwardClick() {
+    this.slides.lockSwipes(false);
+    this.slides.slideNext();
+    this.slides.lockSwipes(true);
+  }
 
-    onSubmitClick() {
-        const data = {
-            name: this.form.value['step0']['name'],
-            age: this.form.value['step1']['age']
-        };
-        this.dataService.saveData(data)
-            .then((result: any) => {
-                this.alertService.show(`Tus datos se han guardado satisfactoriamente: Nombre ${result.name}, Edad: ${result.age}`, '¡Genial!')
-            }, error => {
-                this.alertService.show(error.message, 'Error');
-            });
-    }
+  onSubmitClick() {
+    const data = {
+      name: this.form.value['step0']['name'],
+      age: this.form.value['step1']['age']
+    };
+    this.dataService.saveData(data)
+      .then((result: any) => {
+        this.alertService.show(`Tus datos se han guardado satisfactoriamente: Nombre ${result.name}, Edad: ${result.age}`, '¡Genial!')
+      }, error => {
+        this.alertService.show(error.message, 'Error');
+      });
+  }
 
-    private _createForm() {
-        this.form = this.formBuilder.group({
-            step0: this.formBuilder.group({
-                name: ['', Validators.required],
-            }),
-            step1: this.formBuilder.group({
-                age: ['', Validators.required],
-            })
-        });
-    }
+  private _createForm() {
+    this.form = this.formBuilder.group({
+      step0: this.formBuilder.group({
+        name: ['', Validators.required],
+      }),
+      step1: this.formBuilder.group({
+        age: ['', Validators.required],
+      }),
+      step2: this.formBuilder.group({})
+    });
+  }
 }
